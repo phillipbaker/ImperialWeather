af9dab4c22de53395fb837430a3b0d2a665e7406
Phillip Baker <16352712+phillipbaker@users.noreply.github.com>
2021-06-18 10:43:10 +0100

    Initial commit

---
+++ ImperialWeather/ImperialWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImperialWeatherApp.swift
+//  ImperialWeather
+//
+//  Created by Phillip Baker on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ImperialWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Imperial Weather
+
+A weather app that displays the current temperature in Fahrenheit and Celsius at the same time.
