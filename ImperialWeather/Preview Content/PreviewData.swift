bbbaa80c3ff066155b8226dd95550d26b665d8e4
Phillip Baker <16352712+phillipbaker@users.noreply.github.com>
2022-11-02 14:09:47 +0000

    Update preview data temperatures to make charts more interesting.

--- ImperialWeather/Preview Content/PreviewData.swift
+++ ImperialWeather/Preview Content/PreviewData.swift
@@ -41,13 +41,13 @@ extension HourlyWeather {
 
 extension DailyWeather {
     static let previewData: [DailyWeather] = [
-        .init(day: Date.now, temperature: Temperature(min: 72.0, max: 92.0), description: [WeatherDescription.previewData]),
-        .init(day: Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!, temperature: Temperature(min: 73.0, max: 97.0), description: [WeatherDescription.previewData]),
-        .init(day: Calendar.current.date(byAdding: .day, value: 2, to: Date.now)!, temperature: Temperature(min: 73.0, max: 97.0), description: [WeatherDescription.previewData]),
-        .init(day: Calendar.current.date(byAdding: .day, value: 3, to: Date.now)!, temperature: Temperature(min: 73.0, max: 97.0), description: [WeatherDescription.previewData]),
-        .init(day: Calendar.current.date(byAdding: .day, value: 4, to: Date.now)!, temperature: Temperature(min: 73.0, max: 97.0), description: [WeatherDescription.previewData]),
-        .init(day: Calendar.current.date(byAdding: .day, value: 5, to: Date.now)!, temperature: Temperature(min: 73.0, max: 97.0), description: [WeatherDescription.previewData]),
-        .init(day: Calendar.current.date(byAdding: .day, value: 6, to: Date.now)!, temperature: Temperature(min: 73.0, max: 97.0), description: [WeatherDescription.previewData]),
-        .init(day: Calendar.current.date(byAdding: .day, value: 7, to: Date.now)!, temperature: Temperature(min: 73.0, max: 97.0), description: [WeatherDescription.previewData])
+        .init(day: Date.now, temperature: Temperature(min: 22.0, max: 32.0), description: [WeatherDescription.previewData]),
+        .init(day: Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!, temperature: Temperature(min: 23.0, max: 34.0), description: [WeatherDescription.previewData]),
+        .init(day: Calendar.current.date(byAdding: .day, value: 2, to: Date.now)!, temperature: Temperature(min: 24.0, max: 36.0), description: [WeatherDescription.previewData]),
+        .init(day: Calendar.current.date(byAdding: .day, value: 3, to: Date.now)!, temperature: Temperature(min: 26.0, max: 36.0), description: [WeatherDescription.previewData]),
+        .init(day: Calendar.current.date(byAdding: .day, value: 4, to: Date.now)!, temperature: Temperature(min: 20.0, max: 32.0), description: [WeatherDescription.previewData]),
+        .init(day: Calendar.current.date(byAdding: .day, value: 5, to: Date.now)!, temperature: Temperature(min: 22.0, max: 33.0), description: [WeatherDescription.previewData]),
+        .init(day: Calendar.current.date(byAdding: .day, value: 6, to: Date.now)!, temperature: Temperature(min: 19.0, max: 29.0), description: [WeatherDescription.previewData]),
+        .init(day: Calendar.current.date(byAdding: .day, value: 7, to: Date.now)!, temperature: Temperature(min: 16.0, max: 27.0), description: [WeatherDescription.previewData])
     ]
 }
