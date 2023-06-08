26bcfe1650f63c3aaa14c18765b16f7ad6ce3cb4
Phillip Baker <16352712+phillipbaker@users.noreply.github.com>
2023-06-08 12:25:26 +0100

    Remove API Key.

--- ImperialWeather/Service/Resources/APIResource.swift
+++ ImperialWeather/Service/Resources/APIResource.swift
@@ -29,7 +29,7 @@ extension APIResource {
         
         components.queryItems = [
             URLQueryItem(name: "units", value: "metric"),
-            URLQueryItem(name: "appid", value: "288024cdc4c54aff1f4587c80792e725"),
+            URLQueryItem(name: "appid", value: "INSERT_API_KEY_HERE"),
             URLQueryItem(name: "lat", value: latitude),
             URLQueryItem(name: "lon", value: longitude)
         ]
