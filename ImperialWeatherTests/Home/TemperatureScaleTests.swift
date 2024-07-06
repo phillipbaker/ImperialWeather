fcc631927520f70710ef563db18680378a41d2f2
Phillip Baker <16352712+phillipbaker@users.noreply.github.com>
2024-07-06 07:01:08 +0100

    REFACTOR: Add TemperatureScale tests

---
+++ ImperialWeatherTests/Home/TemperatureScaleTests.swift
@@ -0,0 +1,19 @@
+//
+//  TemperatureScaleTests.swift
+//  ImperialWeather
+//
+//  Created by Phillip Baker on 06/07/2024.
+//
+
+@testable import ImperialWeather
+import Testing
+
+struct TemperatureScaleTests {
+    @Test func idEqualsRawValue() {
+        let celsius = TemperatureScale.celsius
+        #expect(celsius.id == 0)
+        
+        let fahrenheit = TemperatureScale.fahrenheit
+        #expect(fahrenheit.id == 1)
+    }
+}
